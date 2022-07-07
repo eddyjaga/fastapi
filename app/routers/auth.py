2017e7b479ea2044deb6322fa9ab0653e0b74486
Eddy Jaga <eddyjaga@gmail.com>
2022-07-07 22:12:07 +1000

    Tested expired token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -7,7 +7,7 @@ from .. import database, schemas, models, utils, oauth2
 router = APIRouter(tags=["Authentication"])
 
 
-@router.post('/login')
+@router.post('/login', response_model=schemas.Token)
 async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
 
     user = db.query(models.User).filter(
@@ -15,11 +15,11 @@ async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Ses
 
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
 
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
 
     # create  a token
     # return token
